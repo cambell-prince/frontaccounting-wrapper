d68492173ccc8ed87e7884c4c93f9d5a6cb9363e
Cambell <cambell.prince@gmail.com>
2014-09-28 11:29:36 +0700

    Added banking pageElements

--- tests/e2e/banking/pages/bankTransfer.page.js
+++ tests/e2e/banking/pages/bankTransfer.page.js
@@ -2,35 +2,41 @@
 
 var BankTransferPage = function(transactionNo) {
   browser.ignoreSynchronization = true;
+  var page = this;
+
 //  http://bms.local/gl/bank_transfer.php&trans_type=4
   var url = '/gl/bank_transfer.php';
   if (transactionNo) {
     url += '?ModifyTransfer=Yes&trans_no=' + transactionNo;
   }
-  browser.get(url);
 
-  this.fromAccount = element(by.name('FromBankAccount'));
-  this.toAccount = element(by.name('ToBankAccount'));
-  this.date = element(by.name('DatePaid'));
-  this.reference = element(by.name('ref'));
-  this.amount = element(by.name('amount'));
-  this.bankCharge = element(by.name('charge'));
-  this.memo = element(by.name('memo_'));
-  this.submit = element(by.name('submit'));
+  this.pageElements = function() {
+    page.fromAccount = element(by.name('FromBankAccount'));
+    page.toAccount = element(by.name('ToBankAccount'));
+    page.date = element(by.name('DatePaid'));
+    page.reference = element(by.name('ref'));
+    page.amount = element(by.name('amount'));
+    page.bankCharge = element(by.name('charge'));
+    page.memo = element(by.name('memo_'));
+    page.submit = element(by.name('submit'));
+  }
 
   this.transfer = function(from, to, date, amount, memo, bankCharge) {
-    this.fromAccount.element(by.cssContainingText('option', from)).click();
-    this.toAccount.element(by.cssContainingText('option', to)).click();
-    this.date.clear();
-    this.date.sendKeys(date);
-    this.amount.clear();
-    this.amount.sendKeys(amount);
+    page.fromAccount.element(by.cssContainingText('option', from)).click();
+    page.toAccount.element(by.cssContainingText('option', to)).click();
+    page.date.clear();
+    page.date.sendKeys(date);
+    page.amount.clear();
+    page.amount.sendKeys(amount);
     if (memo) {
-      this.memo.clear();
-      this.memo.sendKeys(memo);
+      page.memo.clear();
+      page.memo.sendKeys(memo);
     }
-    if (bankCharge) this.bankCharge.sendKeys(bankCharge);
-    this.submit.click();
+    if (bankCharge) {
+      page.bankCharge.clear();
+      page.bankCharge.sendKeys(bankCharge);
+    }
+    page.submit.click();
   };
 
   this.getTitle = function() {
@@ -38,12 +44,15 @@ var BankTransferPage = function(transactionNo) {
   };
 
   this.getReference = function() {
-    return this.reference.getAttribute('value');
+    return page.reference.getAttribute('value');
   };
 
   this.getNoteMessage = function() {
     return element(by.css('div.note_msg')).getText();
   };
+
+  browser.get(url);
+  this.pageElements();
 
 }
 
