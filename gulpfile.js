6f08f154e7b2df46d786372424225d03c9b3aef8
Cambell <cambell.prince@gmail.com>
2014-12-29 21:04:23 +0700

    Install protractor locally for travis

--- gulpfile.js
+++ gulpfile.js
@@ -212,7 +212,7 @@ gulp.task('start-php', function(cb) {
 
 gulp.task('test-e2e', ['env-test'], function(cb) {
   execute(
-    '/usr/local/bin/protractor tests/e2e/phantom-conf.js',
+    'node ./node_modules/protractor/bin/protractor tests/e2e/phantom-conf.js',
     null,
     cb
   );
